26d9d8b5c83ba3979626034de758c4dc2a574095
10tekinvo@gmail.com <10tekinvo@gmail.com>
2019-12-09 19:17:48 +0600

    Add project files.

---
+++ TreadPoolLesson/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading;
+
+namespace TreadPoolLesson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ThreadPool.GetAvailableThreads(out int threads, out int ports);
+            Console.WriteLine(threads);
+            /*for (var i = 0; i < 20; i++)
+            {
+                ThreadPool.QueueUserWorkItem(ProcessNumbers);
+            }
+            Console.ReadKey();*/
+            //Thread.CurrentThread.Join();
+
+            var timer = new Timer(ProcessNumbers, null, TimeSpan.FromSeconds(1), TimeSpan.FromSeconds(1));
+            Console.ReadLine();
+        }
+
+        static void ProcessNumbers(object state)
+        {
+            Console.Clear();
+            Console.WriteLine(DateTime.Now.ToLongTimeString());
+
+            //var currentThread = Thread.CurrentThread;
+            //for (var i = 0; i <= 10; i++)
+            //{
+            //    Console.WriteLine($"[{currentThread.ManagedThreadId} - {i}]");
+            //    Thread.Sleep(500);
+            //}
+            //Console.WriteLine($"[{currentThread.ManagedThreadId}] - Закончил");
+        }
+    }
+}
